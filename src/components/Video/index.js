86e673dc3744e350bb5211b02aaae038e2e0f640
drd-2rcl <drd.rclrd@gmail.com>
2019-04-19 20:25:06 -0300

    refactoring for send data to connect state redux

--- src/components/Video/index.js
+++ src/components/Video/index.js
@@ -1,12 +1,17 @@
-import React, { Component } from 'react'
+import React from 'react';
 
-export default class Video extends Component {
-  render() {
-    return (
-      <div>
-        <strong>Módulo X</strong>
-        <span> Aula X</span>
-      </div>
-    )
-  }
-}
+import { connect } from 'react-redux';
+
+const Video = ({ activeModule, activeLesson }) => (
+  <div>
+    <strong>Módulo X {activeModule.title}</strong>
+    <span>Aula X {activeLesson.title}</span>
+  </div>
+);
+
+export default connect(
+  state => ({ 
+    activeModule: state.course.activeModule,
+    activeLesson: state.course.activeLesson  
+   })
+)(Video)
