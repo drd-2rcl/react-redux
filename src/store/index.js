f6a534c266b9da5e26b0d40413e000636398a242
drd-2rcl <drd.rclrd@gmail.com>
2019-04-19 18:50:02 -0300

    refactoring for transfer data to store

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,27 @@
 import { createStore } from 'redux';
 
-const store = createStore();
+function reducer() {
+  return [
+    { id: 1, 
+      title: 'Rect na veia', 
+      lessons: 
+      [ 
+        { id: 1, title: 'Primeiro Teste'},
+        { id: 2, title: 'Segundo Teste'},
+      ],
+    },
+    {
+      id: 2, 
+      title: 'Redux na veia',
+      lessons: 
+      [
+        { id: 3, title: 'Terceiro Teste'},
+        { id: 4, title: 'Quarto Teste'},
+      ]
+    }
+  ];
+}
+
+const store = createStore(reducer);
 
 export default store;
