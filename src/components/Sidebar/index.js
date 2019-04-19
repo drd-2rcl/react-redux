83a0170ee641c2e99931f0f42557700fb71c27ce
drd-2rcl <drd.rclrd@gmail.com>
2019-04-19 18:48:33 -0300

    refactoring for stateless component

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,47 +1,22 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-class Sidebar extends Component {
-  state = {
-    modules: [
-      { id: 1, 
-        title: 'Rect na veia', 
-        lessons: 
-        [ 
-          { id: 1, title: 'Primeiro Teste'},
-          { id: 2, title: 'Segundo Teste'},
-        ],
-      },
-      {
-        id: 2, 
-        title: 'Redux na veia',
-        lessons: 
-        [
-          { id: 3, title: 'Terceiro Teste'},
-          { id: 4, title: 'Quarto Teste'},
-        ]
-      }
-    ]
-  }
+import { connect } from 'react-redux';
 
-  render() {
-    const { modules } = this.state;
-    return (
-      <aside>
-        { modules.map(module => (
-          <div hey={module.id} >
-            <strong>{module.title}</strong>
+const Sidebar = ({ modules }) => (
+  <aside>
+    { modules.map(module => (
+      <div hey={module.id} >
+        <strong>{module.title}</strong>
 
-            <ul>
-              { module.lessons.map(lesson => (
-                <li key={lesson.id}>{lesson.title}</li>
-              ))}
-            </ul>
+        <ul>
+          { module.lessons.map(lesson => (
+            <li key={lesson.id}>{lesson.title}</li>
+          ))}
+        </ul>
 
-          </div>
-        ))}
-      </aside>
-    )
-  }
-}
+      </div>
+    ))}
+  </aside>
+)
 
-export default Sidebar
+export default connect(state => ({ modules: state }))(Sidebar);
