778e13571113893fc9c963be72fefc65b6ccc000
drd-2rcl <drd.rclrd@gmail.com>
2019-04-19 20:23:32 -0300

    bindActionCreator, mapStateToProps, mapDispatchToP

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,22 +1,35 @@
 import React from 'react'
-
 import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 
-const Sidebar = ({ modules }) => (
+import * as CourseActions from '../../store/actions/course';
+
+// dispatch serve para disparar actions para o redux, desta forma elas se tornam públicas para a nossa aplicação;
+
+const Sidebar = ({ modules, toggleLesson }) => (
   <aside>
     { modules.map(module => (
       <div hey={module.id} >
         <strong>{module.title}</strong>
-
         <ul>
           { module.lessons.map(lesson => (
-            <li key={lesson.id}>{lesson.title}</li>
+            <li key={lesson.id}>
+              {lesson.title}
+              <button onClick={() => toggleLesson(module, lesson)}>Selecionar
+              </button>
+            </li>
           ))}
         </ul>
-
       </div>
     ))}
   </aside>
 )
 
-export default connect(state => ({ modules: state }))(Sidebar);
+const mapStateToProps = state => ({
+  modules: state.course.modules
+});
+
+const mapDispatchToProps = dispatch => bindActionCreators(CourseActions, dispatch);
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(Sidebar);
